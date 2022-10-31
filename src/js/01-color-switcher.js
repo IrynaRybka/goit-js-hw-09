2daa28224364b38b6fe364f2f21b3c07e8532b2d
IrynaRybka <irarybka@gmail.com>
2022-10-31 13:11:11 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+// const getEl = selector => document.querySelector(selector);
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector("body");
 
+const AMOUNT = 1000;
+let timeoutId = null;
+
+startBtn.addEventListener('click', colorRandom);
+stopBtn.addEventListener('click', stopColorRandom);
+
+// function colorRandom() {   
+//         const changeColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//         bodyRef.style.backgroundColor = changeColor;
+//         return changeColor;
+// }
+function colorRandom() {   
+    timeoutId = setInterval(()=> {
+        const changeColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        bodyRef.style.backgroundColor = changeColor;
+        return changeColor;
+    }, AMOUNT);
+}
+
+function stopColorRandom(){
+    console.log("Bye");
+    clearTimeout(timeoutId);
+}
+
+// timeoutId = setInterval(()=> {
+// console.log('Show color '+ colorRandom())
+// }, AMOUNT);
